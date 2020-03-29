a92a7add469752be49bb62fe5337e45c4d102068
henrique-magana <henriquesmagana@gmail.com>
2020-03-29 18:59:15 -0300

    ola

--- src/course/Program.java
+++ src/course/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Hello World! ");
 		System.out.println("Seu filho da puta do caralho");
+		System.out.println("Ola mundo em pt-br");
 	}
 
 }
